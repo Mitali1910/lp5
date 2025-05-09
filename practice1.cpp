59d8d53206dcb3edca3016d242870af8a7e54d4c
Mitali1910 <153922890+Mitali1910@users.noreply.github.com>
2025-05-09 22:07:59 +0530

    Add files via upload

---
+++ practice1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<omp.h>
+using namespace std;
+int minval(int arr[], int n) {
+    int minval = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < minval)
+            minval = arr[i];
+    }
+    return minval;
+}
+int maxval(int arr[],int n){
+	int maxval=arr[0];
+	#pragma omp parallel for reduction(max:maxval)
+	for(int i=0;i<=n;i++){
+		if(maxval<arr[i]) maxval=arr[i];
+		
+	}
+	return maxval;
+}
+int sum(int arr[],int n){
+	int sum=0;
+	#pragma omp parallel for reduction(+:sum)
+	for(int i=0;i<n;i++){
+		sum=+arr[i];
+	}
+	return sum;
+}
+int avg(int arr[],int n){
+	return (double)sum(arr,n)/n;
+	
+}
+int main(){
+	int n=5;
+	int arr[]={1,2,3,4,5};
+	cout<<"The minimum value is:  "<<minval(arr,n)<< '\n';
+	cout<<"The maximum value is: "<<maxval(arr,n)<< '\n';
+	cout<<"The avg value is: "<<avg(arr,n)<< '\n';
+	cout<<"The sum is: "<<sum(arr,n)<<'\n';
+	return 0;
+}
+
+
